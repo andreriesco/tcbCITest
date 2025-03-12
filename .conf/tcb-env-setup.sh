0219ebe3bdcbb55d825aa8558c0748685a663feb
Andre Riesco <andre.riesco@toradex.com>
2025-03-12 15:38:57 -0300

    kjkj

--- .conf/tcb-env-setup.sh
+++ .conf/tcb-env-setup.sh
@@ -303,7 +303,7 @@ function tcb_dynamic_params() {
 export -f tcb_dynamic_params
 echo $working_directory
 pwd
-alias torizoncore-builder='docker run --rm'"$volumes"' -v '"$working_directory"':/workdir -v '"$storage"':/storage -v /var/run/docker/docker.sock:/var/run/docker/docker.sock'"$network"' '"$(tcb_dynamic_params)"' '"$*"' torizon/torizoncore-builder:'"$chosen_tag"
+alias torizoncore-builder='docker run --user=root --rm'"$volumes"' -v '"$working_directory"':/workdir -v '"$storage"':/storage -v /var/run/docker/docker.sock:/var/run/docker/docker.sock'"$network"' '"$(tcb_dynamic_params)"' '"$*"' torizon/torizoncore-builder:'"$chosen_tag"
 
 [[ $storage =~ ^[a-zA-Z][a-zA-Z0-9_.-]*$ ]] && storage="Docker volume named '$storage'"
 
